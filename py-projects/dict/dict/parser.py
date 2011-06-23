475f15f6e33162d92943ef2c151c97746b5fc7bb
Le Huy <lehuy20@gmail.com>
2011-06-23 11:24:06 +0200

    pass test with sample input

--- py-projects/dict/dict/parser.py
+++ py-projects/dict/dict/parser.py
@@ -1,17 +1,54 @@
 class DictParser:
   def __init__(self):
-    self.defs=[]
+    self.dict={}
 
   def ignored(self,str):
     str = str.strip()
     return str == "#" or str == ""
 
   def parse(self,lines):
-    self.defs = [z for z in lines.split("\n") if not self.ignored(z)]
+    words_used_in_def=set()
+    lp = LineParser()
+    for z in lines.split("\n"):
+      if not self.ignored(z):
+        lp.parse(z)
+        self.dict[lp.word] = lp.definition
+        words_used_in_def = words_used_in_def.union(lp.definition)
 
-class WordDef:
+    self.not_used_in_any_defs = [z for z in self.dict if not z in words_used_in_def] 
 
-  def __init__(self,line):
+    return self
+
+  def num_words(self):
+    return len(self.dict.keys())
+
+class LineParser:
+
+  def parse(self,line):
     word_arr = line.split("#")[0].strip().split()
     self.word = word_arr[0]
-    self.definitions = [z for z in word_arr[1:] if z != "."]
+    self.definition = [z for z in word_arr[1:] if z != "."]
+    return self
+
+class SortError(Exception): 
+  pass 
+
+class TSorter:
+
+  def __init__(self,dict):
+    self.ordered = []
+    self.dict = dict
+
+  def sort(self,start_with):
+    visited = set([])
+    for z in start_with:
+      self.visit(z,visited)
+    return self
+      
+  def visit(self,word,visited):
+    if not word in visited:
+      visited.add(word)
+      for z in self.dict[word]:
+        self.visit(z,visited)
+      self.ordered.append(word) 
+
--- py-projects/dict/tests/dict_tests.py
+++ py-projects/dict/tests/dict_tests.py
@@ -3,28 +3,69 @@ from dict.parser import *
 
 class TestDictParser:
 
-  def setup(self):
-    self.parser = DictParser()
-
-  def teardown(self):
-    pass
-  
   def test_ignore_comment(self):
-    self.parser.parse("#\n #\n\t#\nColor .") 
-    assert_equal(1,len(self.parser.defs))
+    p = DictParser().parse("#\n #\n\t#\nColor .") 
+    assert_equal(1,p.num_words())
 
   def test_ignore_empty_line(self):
-    self.parser.parse("\n \n\t\nColor .") 
-    assert_equal(1,len(self.parser.defs))
+    p = DictParser().parse("\n \n\t\nColor .") 
+    assert_equal(1,p.num_words())
 
-class TestWordDef:
+  def test_words_not_used_in_any_defs(self):
+    p = DictParser().parse("Color .\nRed Color\nFruit .\nApple Red Fruit") 
+    assert_equal(["Apple"],p.not_used_in_any_defs)
 
-  def test_word(self):
-    assert_equal("Yellow",WordDef("Yellow Color").word)
+
+class TestLineParser:
 
   def test_ignore_comment(self):
-    assert_equal(["Color"],WordDef("Yellow Color # bright").definitions)
+    p = LineParser().parse("Yellow Color # bright")
+    assert_equal(["Color"],p.definition)
 
   def test_primitive_def(self):
-    assert_equal([],WordDef("Color .").definitions)
+    p = LineParser().parse("Color .")
+    assert_equal([],p.definition)
 
+  def test_word(self):
+    p = LineParser().parse("Yellow Color")
+    assert_equal("Yellow",p.word)
+
+  def test_def(self):
+    p = LineParser().parse("Apple Red Fruit")
+    assert_equal(["Red","Fruit"],p.definition)
+
+class TestTSorter:
+
+  def test_only_primitives(self):
+    ts = TSorter({"Color":[],"Fruit":[]}).sort(["Color","Fruit"])
+    assert_equal(["Color","Fruit"],ts.ordered)
+    
+  def test_distance_zero(self):
+    ts = TSorter({"Color":[],"Yellow":["Color"]}).sort(["Yellow"])
+    assert_equal(["Color","Yellow"],ts.ordered)
+
+  def test_distance_one(self):
+    ts = TSorter({"Color":[],"Yellow":["Color"],"Fruit":[],
+                 "Pear":["Yellow","Fruit"]}).sort(["Pear"])
+    assert_equal(["Color","Yellow","Fruit","Pear"],ts.ordered)
+
+  def test_has_cycle(self):
+    ts = TSorter({"Egg":["Chicken","White"],"Chicken":["Egg","Yellow"],
+                  "Pear":["Yellow"],"Yellow":["Color"],"White":["Color"],"Color":[]}).sort(["Pear"])
+    assert_equal(["Color","Yellow","Pear"],ts.ordered)
+    
+class TestDictSort:
+
+   def test_sample_input(self):
+     p = DictParser().parse(
+"""
+Apple    Red Fruit    # Hey, this is the first line 
+Red      Color 
+Yellow   Color 
+Color    .    # This is a primitive 
+Fruit    . 
+Pear     Yellow Fruit 
+"""
+     )
+     ts = TSorter(p.dict).sort(p.not_used_in_any_defs)
+     assert_equal(["Color","Red","Fruit","Apple","Yellow","Pear"],ts.ordered)
